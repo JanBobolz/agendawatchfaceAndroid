21acf213a6f275a3e44fc0ce474b201b91397bbd
JanBobolz <mail@jan-bobolz.de>
2014-10-14 21:58:22 +0200

    Moved countdown setting to calendar plugin

--- src/de/janbo/agendawatchface/calendar/AgendaCalendarProvider.java
+++ src/de/janbo/agendawatchface/calendar/AgendaCalendarProvider.java
@@ -21,6 +21,17 @@ public class AgendaCalendarProvider extends AgendaWatchfacePlugin {
 	@Override
 	public void onRefreshRequest(Context context) {
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+		
+		//Apply old global pref_layout_countdown setting if it's set from an old version of this app. Only done once.
+		if (prefs.contains("pref_layout_countdown")) {
+			boolean state = prefs.getBoolean("pref_layout_countdown", false);
+			prefs.edit()
+				.putBoolean("pref_layout_countdown_1", state)
+				.putBoolean("pref_layout_countdown_2", state)
+				.remove("pref_layout_countdown")
+				.commit();
+		}
+		
 		if (!prefs.getBoolean("pref_key_cal_activate", true))
 			return;
 		context.startService(new Intent(context, AgendaCalendarService.class));
